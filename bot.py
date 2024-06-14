7c53263b5881f032ddc99ab515152109ddecab32
duranvilay <DDo000@sea.yearup.org>
2024-06-14 09:42:47 -0700

    update6

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# This script imports necessary modules for a Discord bot and retrieves environmental variables.
+#Import necessary modules for Discord bot.
 import discord
 import os
 import random
@@ -14,14 +14,14 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 token = str(os.getenv('TOKEN'))
 
-# Event handler triggered when the bot has successfully connected to Discord.
+#Event handler triggered when the bot has successfully connected to the Discord server.
 @client.event #<--Fix
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
     print(f'This is my Ec2_metadata.region:', ec2_metadata.region)
     print(f'This is my Ec2_metadata.instance.id:', ec2_metadata.instance_id)
 
-# Event handler triggered whenever a message is sent in a channel the bot has access to.
+#Event handler triggered when a message is sent in a channel the bot has access to.
 @client.event
 async def on_message(message):
     username = str(message.author).split("#")[0]
@@ -31,22 +31,22 @@ async def on_message(message):
     
     print(f'Message {user_message} by {username} on {channel}')
 
-    # Check if the message author is the bot itself, if so, ignore the message.
+    #Check who the message was sent from.
     if message.author == client.user:
         return 
     
-    # Check if the message was sent in the channel named "hi".
+    #Check if the message was sent in the channel named "hi".
     if channel == "hi":
-        # If the message content is "test" or "test", send a message with EC2 region data.
+        #If the message is "test", send response.
         if user_message.lower() == "test" or user_message.lower() == "test":
             await message.channel.send(f"okay! {username} Your EC2 Data: {ec2_metadata.region}")
             return 
         
-        # If the message content is "hello?", respond with a greeting.
+        #If the message is "hello?", send response.
         elif user_message.lower() == "hello?":
             await message.channel.send(f'Sooner! {username}')
-            
-          # If the message content is "ec2 data", send the EC2 instance data.
+
+        #If the message is "ec2 data", send response.
         elif user_message.lower() == "ec2 data":
             await message.channel.send("Your instance data is" + ec2_metadata.region)
         
